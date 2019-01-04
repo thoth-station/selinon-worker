ddd346e60dbf89988d42417363ed26648d67d369
Fridolin Pokorny <fridolin@redhat.com>
2019-01-04 09:53:23 +0100

    Do not forget to include MANIFEST.in

--- thoth/worker/__init__.py
+++ thoth/worker/__init__.py
@@ -18,7 +18,7 @@
 """Selinon worker for Thoth to perform data aggregation and analysis for high volume data (parallel processing)."""
 
 __name__ = "thoth.worker"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "Fridolin Pokorny"
 
 from .utils import get_config_files
